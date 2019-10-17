de49d6f594e6f15adcfc998a8164b914752bfb0c
John Byrne <john@trinnylondon.com>
2019-10-17 16:08:48 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var { graphql, buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
+
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
